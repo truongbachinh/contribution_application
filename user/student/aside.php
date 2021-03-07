660dc00923c851e070d226832bafc89e6ccb5624
Nguyen  Thu Thuy <haducc246@gmail.com>
2021-03-07 22:13:54 +0700

    Student role

---
+++ user/student/aside.php
@@ -0,0 +1,63 @@
+<aside class="admin-sidebar">
+    <div class="admin-sidebar-brand">
+        <!-- begin sidebar branding-->
+        <img class="admin-brand-logo" >LOGO TEAM </img>
+        <!-- end sidebar branding-->
+        <div class="ml-auto">
+            <!-- sidebar pin-->
+            <a href="#" class="admin-pin-sidebar btn-ghost btn btn-rounded-circle"></a>
+            <!-- sidebar close for mobile device-->
+            <a href="#" class="admin-close-sidebar"></a>
+        </div>
+    </div>
+    <div class="admin-sidebar-wrapper js-scrollbar">
+        <ul class="menu">
+            <li class="menu-item active ">
+                <a href="#" class="menu-link">
+                    <span class="menu-label">
+                        <span class="menu-name">Homescreen
+                        </span>
+                    </span>
+                    <span class="menu-icon">
+                        <i class="icon-placeholder fe fe-activity "></i>
+                    </span>
+                </a>
+            </li>
+            <li class="menu-item ">
+                <a href="#" class="open-dropdown menu-link">
+                    <span class="menu-label">
+                        <span class="menu-name">Dashboard
+                        </span>
+
+                    </span>
+                    <span class="menu-icon">
+                        <i class="icon-placeholder fe fe-edit "></i>
+                    </span>
+                </a>
+            </li>
+            <li class="menu-item ">
+                <a href="#" class="open-dropdown menu-link">
+                    <span class="menu-label">
+                        <span class="menu-name">Calendar
+                        </span>
+                    </span>
+                    <span class="menu-icon">
+                        <i class="icon-placeholder fe fe-calendar"></i>
+                    </span>
+                </a>
+            </li>
+            <li class="menu-item ">
+                <a href="#" class="open-dropdown menu-link">
+                    <span class="menu-label">
+                        <span class="menu-name">My Faculty
+                        </span>
+                    </span>
+                    <span class="menu-icon">
+                        <i class="icon-placeholder fe fe-folder"></i>
+                    </span>
+                </a>
+            </li>
+        
+        </ul>
+    </div>
+</aside>
