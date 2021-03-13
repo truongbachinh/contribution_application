b562063f42e54cc0af84540ba83f12542673224d
truongbachinh <58037395+truongbachinh@users.noreply.github.com>
2021-03-13 21:15:28 +0700

    a

--- user/student/header.php
+++ user/student/header.php
@@ -1,11 +1,15 @@
-<link rel="stylesheet" href="./css/student.css"/>
+<?php
+include "connect_db.php";
+
+?>
+<link rel="stylesheet" href="./css/student.css" />
 <header class="admin-header">
     <a href="#" class="sidebar-toggle" data-toggleclass="sidebar-open" data-target="body"> </a>
     <nav class=" ml-auto">
         <ul class="nav align-items-center m-r-30">
             <li class=nav-item>
-            <div class="d-flex p-all-15  justify-content-between">
-                <a href="#!" class="nar-link" ><i class="mdi mdi-24px mdi-chat"></i>
+                <div class="d-flex p-all-15  justify-content-between">
+                    <a href="#!" class="nar-link"><i class="mdi mdi-24px mdi-chat"></i>
                         <!-- <span class="notification-counter"></span></a> -->
             </li>
             <li class="nav-item">
@@ -43,28 +47,76 @@
                 </div>
             </li>
             <li class="nav-item dropdown ">
-                <a class="nav-link dropdown-toggle" href="#"  role="button" data-toggle="dropdown"
-                   aria-haspopup="true" aria-expanded="false">
-                   <div class="d-inline-flex">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <div class="d-inline-flex">
                         <div class="avatar avatar-sm avatar-online mr-3">
-                             <span class="avatar-title rounded-circle bg-dark">T</span>
+                            <span class="avatar-title rounded-circle bg-dark">T</span>
                         </div>
-                         <div class="d-flex flex-column">
-                                <div class="name-student">Nguyen Thu Thuy</div>
-                                <div class="role">Student Account</div>  
-                        </div>
-                   </div>            
+
+                        <?php
+                        if (!empty($_SESSION["current_user"]["username"])) {
+
+
+                        ?>
+                            <div class="d-flex flex-column">
+                                <div class="name-student"><?php echo $_SESSION["current_user"]["username"] ?></div>
+                                <div class="role">Student Account</div>
+                            </div>
+                            <div class="dropdown-menu  dropdown-menu-right">
+                                <a class="dropdown-item" onclick="profileButton()"> Profile
+                                </a>
+                                <a class="dropdown-item" href="./change_password.php"> Reset Password</a>
+                                <a class="dropdown-item" href=""> Help </a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" onclick="logoutButton()"> Logout</a>
+                            </div>
+                        <?php
+                        } elseif (!empty($_SESSION["current_user_goole"]["fullname"])) {
+                        ?>
+                            <div class="d-flex flex-column">
+                                <div class="name-student"><?php echo $_SESSION["current_user_goole"]["fullnames"] ?></div>
+                                <div class="role">Student Account</div>
+                            </div>
+                            <div class="dropdown-menu  dropdown-menu-right">
+                                <a class="dropdown-item" onclick="profileButton()"> Profile
+                                </a>
+                                <a class="dropdown-item" href=""> Help </a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" onclick="logoutButton()"> Logout</a>
+                            </div>
+                        <?php
+                        } else {
+
+
+                        ?>
+                            <div class="d-flex flex-column">
+                                <div class="name-student">Guest</div>
+
+                            </div>
+                            <div class="dropdown-menu  dropdown-menu-right">
+                                <a class="dropdown-item" onclick="loginButton()"> Login</a>
+                            </div>
+                        <?php
+                        }
+                        ?>
+                    </div>
                 </a>
-                <div class="dropdown-menu  dropdown-menu-right"   >
-                    <a class="dropdown-item" href="#">  Profile
-                    </a>
-                    <a class="dropdown-item" href="#">  Reset Password</a>
-                    <a class="dropdown-item" href="#">  Help </a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#"> Logout</a>
-                </div>
+
             </li>
         </ul>
 
     </nav>
 </header>
+<script type="text/javascript">
+    function logoutButton() {
+        window.location = "https://ciliweb.vn/a/contribution_application/account/logout.php";
+    }
+
+    function loginButton() {
+        window.location = "https://ciliweb.vn/a/contribution_application/account/login.php";
+    }
+
+    function profileButton() {
+        window.location = "https://ciliweb.vn/a/contribution_application/user/student/profile.php";
+    }
+</script>
--- user/student/homepage.php
+++ user/student/homepage.php
@@ -1,3 +1,8 @@
+<?php
+include "connect_db.php";
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <meta charset="UTF-8">
@@ -6,41 +11,30 @@
 <meta name="apple-touch-fullscreen" content="yes">
 <meta name="apple-mobile-web-app-status-bar-style" content="default">
 <meta content="" name="author" />
-<meta content="atlas is Bootstrap 4 based admin panel.It comes with 100's widgets,charts and icons"
-    name="description" />
+<meta content="atlas is Bootstrap 4 based admin panel.It comes with 100's widgets,charts and icons" name="description" />
 <meta property="og:locale" content="en_US" />
 <meta property="og:type" content="website" />
-<meta property="og:title"
-    content="atlas is Bootstrap 4 based admin panel.It comes with 100's widgets,charts and icons" />
-<meta property="og:description"
-    content="atlas is Bootstrap 4 based admin panel.It comes with 100's widgets,charts and icons" />
-<meta property="og:image"
-    content="https://cdn.dribbble.com/users/180706/screenshots/5424805/the_sceens_-_mobile_perspective_mockup_3_-_by_tranmautritam.jpg" />
+<meta property="og:title" content="atlas is Bootstrap 4 based admin panel.It comes with 100's widgets,charts and icons" />
+<meta property="og:description" content="atlas is Bootstrap 4 based admin panel.It comes with 100's widgets,charts and icons" />
+<meta property="og:image" content="https://cdn.dribbble.com/users/180706/screenshots/5424805/the_sceens_-_mobile_perspective_mockup_3_-_by_tranmautritam.jpg" />
 <meta property="og:site_name" content="atlas " />
 <title>Home Page</title>
 <link rel="icon" type="image/x-icon" href="assets/img/logo.png" />
 <link rel="icon" href="assets/img/logo.png" type="image/png" sizes="16x16">
-<link rel='stylesheet'
-    href='https://d33wubrfki0l68.cloudfront.net/css/478ccdc1892151837f9e7163badb055b8a1833a5/light/assets/vendor/pace/pace.css' />
-<script
-    src='https://d33wubrfki0l68.cloudfront.net/js/3d1965f9e8e63c62b671967aafcad6603deec90c/light/assets/vendor/pace/pace.min.js'></script>
+<link rel='stylesheet' href='https://d33wubrfki0l68.cloudfront.net/css/478ccdc1892151837f9e7163badb055b8a1833a5/light/assets/vendor/pace/pace.css' />
+<script src='https://d33wubrfki0l68.cloudfront.net/js/3d1965f9e8e63c62b671967aafcad6603deec90c/light/assets/vendor/pace/pace.min.js'></script>
 <!--vendors-->
-<link rel='stylesheet' type='text/css'
-    href='https://d33wubrfki0l68.cloudfront.net/bundles/291bbeead57f19651f311362abe809b67adc3fb5.css' />
-<link rel='stylesheet'
-    href='https://d33wubrfki0l68.cloudfront.net/bundles/fc681442cee6ccf717f33ccc57ebf17a4e0792e1.css' />
+<link rel='stylesheet' type='text/css' href='https://d33wubrfki0l68.cloudfront.net/bundles/291bbeead57f19651f311362abe809b67adc3fb5.css' />
+<link rel='stylesheet' href='https://d33wubrfki0l68.cloudfront.net/bundles/fc681442cee6ccf717f33ccc57ebf17a4e0792e1.css' />
 
 
 <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,600" rel="stylesheet">
 <!--Material Icons-->
-<link rel='stylesheet' type='text/css'
-    href='https://d33wubrfki0l68.cloudfront.net/css/548117a22d5d22545a0ab2dddf8940a2e32c04ed/default/assets/fonts/materialdesignicons/materialdesignicons.min.css' />
+<link rel='stylesheet' type='text/css' href='https://d33wubrfki0l68.cloudfront.net/css/548117a22d5d22545a0ab2dddf8940a2e32c04ed/default/assets/fonts/materialdesignicons/materialdesignicons.min.css' />
 <!--Material Icons-->
-<link rel='stylesheet' type='text/css'
-    href='https://d33wubrfki0l68.cloudfront.net/css/0940f25997c8e50e65e95510b30245d116f639f0/light/assets/fonts/feather/feather-icons.css' />
+<link rel='stylesheet' type='text/css' href='https://d33wubrfki0l68.cloudfront.net/css/0940f25997c8e50e65e95510b30245d116f639f0/light/assets/fonts/feather/feather-icons.css' />
 <!--Bootstrap + atmos Admin CSS-->
-<link rel='stylesheet' type='text/css'
-    href='https://d33wubrfki0l68.cloudfront.net/css/16e33a95bb46f814f87079394f72ef62972bd197/light/assets/css/atmos.min.css' />
+<link rel='stylesheet' type='text/css' href='https://d33wubrfki0l68.cloudfront.net/css/16e33a95bb46f814f87079394f72ef62972bd197/light/assets/css/atmos.min.css' />
 <!-- Additional library for page -->
 
 <body class="sidebar-pinned ">
@@ -55,9 +49,7 @@
                 <div class="row">
                     <div class="col-md-12 p-15">
                         <div class="card m-b-30">
-                            <img class="card-img-top"
-                                src="https://www.balmerlawrie.com/img/main_images/inside_banner/travel-banner1.jpg"
-                                alt="Card image cap">
+                            <img class="card-img-top" src="https://www.balmerlawrie.com/img/main_images/inside_banner/travel-banner1.jpg" alt="Card image cap">
                         </div>
                     </div>
                 </div>
@@ -81,8 +73,7 @@
                                             </div>
                                         </div>
                                         <div class="progress" style="height: 5px">
-                                            <div class="progress-bar" role="progressbar" style="width: 37%"
-                                                aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+                                            <div class="progress-bar" role="progressbar" style="width: 37%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
                                         </div>
                                         <div class="row m-t-10">
                                             <div class="col-md-7">
@@ -109,8 +100,7 @@
                                             </div>
                                         </div>
                                         <div class="progress" style="height: 5px">
-                                            <div class="progress-bar" role="progressbar" style="width: 37%"
-                                                aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+                                            <div class="progress-bar" role="progressbar" style="width: 37%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
                                         </div>
                                         <div class="row m-t-10">
                                             <div class="col-md-7">
@@ -137,8 +127,7 @@
                                             </div>
                                         </div>
                                         <div class="progress" style="height: 5px">
-                                            <div class="progress-bar" role="progressbar" style="width: 37%"
-                                                aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+                                            <div class="progress-bar" role="progressbar" style="width: 37%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
                                         </div>
                                         <div class="row m-t-10">
                                             <div class="col-md-7">
@@ -165,8 +154,7 @@
                                             </div>
                                         </div>
                                         <div class="progress" style="height: 5px">
-                                            <div class="progress-bar" role="progressbar" style="width: 37%"
-                                                aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+                                            <div class="progress-bar" role="progressbar" style="width: 37%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
                                         </div>
                                         <div class="row m-t-10">
                                             <div class="col-md-7">
@@ -208,14 +196,20 @@
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-66116118-3"></script>
-<script> window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); } gtag('js', new Date()); gtag('config', 'UA-66116118-3');</script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'UA-66116118-3');
+</script>
 
 <!--Additional Page includes-->
-<script
-    src='https://d33wubrfki0l68.cloudfront.net/js/c36248babf70a3c7ad1dcd98d4250fa60842eea9/light/assets/vendor/apexchart/apexcharts.min.js'></script>
+<script src='https://d33wubrfki0l68.cloudfront.net/js/c36248babf70a3c7ad1dcd98d4250fa60842eea9/light/assets/vendor/apexchart/apexcharts.min.js'></script>
 <!--chart data for current dashboard-->
-<script
-    src='https://d33wubrfki0l68.cloudfront.net/js/d678dabfdc5c3131d492af7ef517fbe46fbbd8e4/light/assets/js/dashboard-01.js'></script>
+<script src='https://d33wubrfki0l68.cloudfront.net/js/d678dabfdc5c3131d492af7ef517fbe46fbbd8e4/light/assets/js/dashboard-01.js'></script>
 
 </body>
 
