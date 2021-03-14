e0c5fb2499592cb1b22f2b7b6437b2ead437fb3f
kimmguyen <nguyendtkgbh17526@fpt.edu.vn>
2021-03-14 23:04:07 +0700

    nguyen day code ngay 14/02

---
+++ user/manage-coordinator/reportPage.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Dashboard">
+    <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+    <title>Report Page</title>
+    <link href="./reportpage/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href=".eportpage/lib/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="./reportpage/css/zabuto_calendar.css">
+    <link rel="stylesheet" type="text/css" href="./reportpage/lib/gritter/css/jquery.gritter.css" />
+    <link href="./reportpage/css/style.css" rel="stylesheet">
+    <link href="./reportpage/css/style-responsive.css" rel="stylesheet">
+    
+</head>
+
+<body>
+    <section id="container">
+        <header class="header black-bg row" style="background-color: aliceblue;">
+            <div class="col-md-6 col-lg-6">
+                <h3 style="text-align: left; margin-top: 2%; margin-left: 2%;  font-weight: bold;">LOGOTEAM</h3>
+            </div>
+
+            <div class="col-md-6 col-lg-6 row" style="text-align: right; ">
+                <div class="col-md-6 col-lg-6">
+
+                </div>
+
+                <div class="col-md-6 row" style="padding: 0px !important;">
+                    <div class="col-md-2" style="margin-top: 4%;">
+                        <img style="width: 53%;" src="./reportpage/img/notification.png">
+                    </div>
+                    <div class="col-md-2" style="margin-top: 4%;">
+                        <img style="width: 53%;" src="./reportpage/img/envelope.png">
+                    </div>
+                    <div class="col-md-2" style="padding: 0px !important; margin-top: 3%; ">
+                        <img style="width: 50%" src="./reportpage/img/Avatar.png">
+                    </div>
+                    <div class="col-md-6 " style="padding: 0px; ">
+                        <h5 style="margin-top: 4%; font-weight: bold; margin-right: 2%; ">Nguyen Thu Thuy</h4>
+                            <h6 style="margin-top: 0%; font-weight: bold; margin-right: 11%; ">Student Account</h6>
+                    </div>
+
+                </div>
+            </div>
+        </header>
+
+        <section>
+            <section class="wrapper ">
+                <div class="row ">
+                    <div class="col-lg-9 main-chart ">
+                        <img style="width: 100% " src="./reportpage/img/background.jpg ">
+                    </div>
+
+                    <div class="col-lg-3 ds " style="position: fixed; right: 0; z-index: 1;height: 100%; ">
+                        <!--COMPLETED ACTIONS DONUTS CHART-->
+                        <div class="donut-main row ">
+                            <div class=" col-lg-12 text-right ">
+                                <i class="fa fa-times " style="font-size: 20px; "></i>
+                            </div>
+                            <div class=" col-lg-12 text-left">
+                                <div class=" col-lg-4 text-center">
+                                    <img src="./reportpage/img/Rectangle 54.png ">
+                                </div>
+                                <div class="col-lg-8 ">
+                                    <h4 style="color: #000; ">Nguyen Minh Phong</h3>
+                                        <h5 style="color: #000; margin-top: 0; ">Topic Cloud computing</h4>
+                                </div>
+                            </div>
+                            <div class=" col-lg-12 text-left ">
+                                <h3 style="font-weight: bolder; color: #000; ">Submission</h3>
+                            </div>
+                            <div class=" col-lg-12 text-left ">
+                                <div style="background-color: #06D6A0; height: 40px; ">
+                                    <div style="margin-top: 4%; font-size: 18px; font-weight: 400;color: #000; padding-top: 10px !important; ">
+                                        Submitted for approved</div>
+                                </div>
+                            </div>
+                            <div class=" col-lg-12 text-left ">
+                                <div style="color: #000;margin-top: 5%; font-size: 18px; font-weight: 400; ">Student cannot edit this submission
+                                </div>
+                            </div>
+                            <div class=" col-lg-12 text-left row ">
+                                <div class=" col-lg-12 text-left row " style="padding-left: 11%; ">
+                                    <hr class=" col-lg-4 " style="width: 2% ">
+                                    </hr>
+                                    <div class=" col-lg-8 " style="color: #000;margin-top: 2%; font-size: 15px; font-weight: 400; ">V1.0 PHONG98 - Validation Phong</div>
+                                </div>
+                                <div class=" col-lg-12 text-left row " style="padding-left: 11%; ">
+                                    <hr class=" col-lg-4 " style="width: 2% ">
+                                    </hr>
+                                    <div class=" col-lg-8 " style="color: #000;margin-top: 2%; font-size: 15px; font-weight: 400; ">V1.0 PHONG98 - Validation Phong</div>
+                                </div>
+                            </div>
+                            <div class=" col-lg-12 text-left " style="background-color: aliceblue; margin-top:7%; width: 90%; margin-left: 5%; border-radius: 15px ">
+                                <h3>Feedback</h3>
+                                <textarea style="width: 100%; height: 180px; border-radius: 3px; border-radius: 14px; resize: none; margin-bottom: 7% "></textarea>
+                            </div>
+                            <div class=" col-lg-12 text-right " style="margin-top: 4%; ">
+                                <div class="col-lg-6 ">
+                                    <button class="btn btn-primary ">Approved</button>
+                                </div>
+                                <div class="col-lg-6 text-left ">
+                                    <button class="btn btn-danger ">Reject</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </section>
+        </section>
+
+        <footer class="site-footer " style="width: 75%; ">
+            <div class="text-center ">
+                <p>
+                    &copy; Copyrights <strong>Dashio</strong>. All Rights Reserved
+                </p>
+                <div class="credits ">
+                    Created with Dashio template by <a href="# ">TemplateMag</a>
+                </div>
+                <a href="index.html# " class="go-top ">
+                    <i class="fa fa-angle-up "></i>
+                </a>
+            </div>
+        </footer>
+    </section>
+
+    <script src="./reportpage/lib/jquery/jquery.min.js "></script>
+    <script src="./reportpage/lib/bootstrap/js/bootstrap.min.js "></script>
+    <script class="include " type="text/javascript " src="./reportpage/lib/jquery.dcjqaccordion.2.7.js "></script>
+    <script src="./reportpage/lib/jquery.scrollTo.min.js "></script>
+    <script src="./reportpage/lib/jquery.nicescroll.js " type="text/javascript "></script>
+    <script src="./reportpage/lib/jquery.sparkline.js "></script>
+    <script src="./reportpage/lib/common-scripts.js "></script>
+    <script type="text/javascript " src="./reportpage/lib/gritter/js/jquery.gritter.js "></script>
+    <script type="text/javascript " src="./reportpage/lib/gritter-conf.js "></script>
+    <script src="./reportpage/lib/sparkline-chart.js "></script>
+    <script src="./reportpage/lib/zabuto_calendar.js "></script>
+</body>
+
+</html>
---
+++ user/student/asideadmin.php
@@ -0,0 +1,69 @@
+<aside class="admin-sidebar">
+    <div class="admin-sidebar-brand">
+        <!-- begin sidebar branding-->
+        <img class="admin-brand-logo">LOGO TEAM </img>
+        <!-- end sidebar branding-->
+        <div class="ml-auto">
+            <!-- sidebar pin-->
+            <a href="#" class="admin-pin-sidebar btn-ghost btn btn-rounded-circle"></a>
+            <!-- sidebar close for mobile device-->
+            <a href="#" class="admin-close-sidebar"></a>
+        </div>
+    </div>
+    <div class="admin-sidebar-wrapper js-scrollbar">
+        <ul class="menu">
+            <li class="menu-item">
+                <a href="#" class="dropdown-toggle menu-link " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <span class="menu-label">
+                        <span class="menu-name">Homescreen
+                        </span>
+                    </span>
+                    <span class="menu-icon">
+                        <i class="icon-placeholder fe fe-chevron-down"></i>
+                    </span>
+                    <div class="dropdown-menu col-md-12">
+                        <a class="dropdown-item" href="#">Manage User</a>
+                        <a class="dropdown-item" href="#">Manage Faculty</a>
+                        <a class="dropdown-item" href="#">Manage Topic</a>
+                        <a class="dropdown-item" href="#">Manage Article</a>
+                    </div>
+                </a>
+            </li>
+            <li class="menu-item ">
+                <a href="#" class="dropdown-toggle menu-link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <span class="menu-label">
+                        <span class="menu-name">Dashboard
+                        </span>
+
+                    </span>
+                    <span class="menu-icon">
+                        <i class="icon-placeholder fe fe-chevron-down"></i>
+                    </span>
+                    <div class="dropdown-menu col-md-12">
+                        <a class="dropdown-item" href="#">Manage User</a>
+                        <a class="dropdown-item" href="#">Manage Faculty</a>
+                        <a class="dropdown-item" href="#">Manage Topic</a>
+                        <a class="dropdown-item" href="#">Manage Article</a>
+                    </div>
+                </a>
+            </li>
+            <li class="menu-item ">
+                <a href="#" class="dropdown-toggle menu-link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <span class="menu-label">
+                        <span class="menu-name">Manage system
+                        </span>
+                    </span>
+                    <span class="menu-icon">
+                        <i class="icon-placeholder fe fe-chevron-down"></i>
+                    </span>
+                    <div class="dropdown-menu col-md-12">
+                        <a class="dropdown-item" href="#">Manage User</a>
+                        <a class="dropdown-item" href="#">Manage Faculty</a>
+                        <a class="dropdown-item" href="#">Manage Topic</a>
+                        <a class="dropdown-item" href="#">Manage Article</a>
+                    </div>
+                </a>
+            </li>
+        </ul>
+    </div>
+</aside>
